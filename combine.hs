4e7b6d6703056c7e0a552f3c0237c02eb50b2247
Jeremy Hicks <jeremy@thehickses.org.uk>
2024-12-02 16:51:00 +0000

    Remove unnecessary constraint

--- combine.hs
+++ combine.hs
@@ -2,7 +2,7 @@ module Combine where
 
 import Data.List
 
-combineMinMaxLength :: (Eq a) => Int -> Int -> [a] -> [[a]]
+combineMinMaxLength :: Int -> Int -> [a] -> [[a]]
 combineMinMaxLength 0 max xs = [] : combineMinMaxLength 1 max xs
 combineMinMaxLength _ _ [] = []
 combineMinMaxLength _ 0 _ = []
@@ -10,14 +10,14 @@ combineMinMaxLength min max xs =
     let combineAt n = (xs !! n :) <$> combineMinMaxLength (min - 1) (max - 1) (drop (n + 1) xs)
     in concatMap combineAt $ take (length xs) [0 ..]
 
-combineAll :: (Eq a) => [a] -> [[a]]
+combineAll :: [a] -> [[a]]
 combineAll xs = combineMinMaxLength 1 (length xs) xs
 
-combineMinLength :: (Eq a) => Int -> [a] -> [[a]]
+combineMinLength :: Int -> [a] -> [[a]]
 combineMinLength min xs = combineMinMaxLength min (length xs) xs
 
-combineMaxLength :: (Eq a) => Int -> [a] -> [[a]]
+combineMaxLength :: Int -> [a] -> [[a]]
 combineMaxLength = combineMinMaxLength 1
 
-combineExactLength :: (Eq a) => Int -> [a] -> [[a]]
+combineExactLength :: Int -> [a] -> [[a]]
 combineExactLength len = combineMinMaxLength len len
