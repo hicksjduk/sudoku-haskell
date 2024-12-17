f6d2987240500ecc9868043ba247cec05dfe6134
Jeremy Hicks <jeremy@thehickses.org.uk>
2024-12-17 16:37:35 +0000

    More run options

--- app/Main.hs
+++ app/Main.hs
@@ -2,14 +2,33 @@ module Main (main) where
 
 import Sudoku
 import System.Environment
+import Data.Foldable
 
 main :: IO ()
 main = do
   args <- getArgs
-  putStrLn $ show $ sudoku $ puzzleSelect args
+  putStrLn $ show $ solve $ puzzleSelect $ tail $ toList args
 
-puzzleSelect :: Foldable t => t String -> Puzzle
-puzzleSelect args
-  | "k" `elem` args = killerPuzzle
-  | "e" `elem` args = SudokuPuzzle emptyGrid
-  | otherwise = puzzle
+puzzleSelect :: [String] -> Puzzle
+puzzleSelect [] = puzzle
+puzzleSelect ("k":_) = killerPuzzle
+puzzleSelect ("e":_) = SudokuPuzzle emptyGrid
+puzzleSelect (a:_) = let i = read a :: Int
+  in partialSolutions !! i
+
+solution :: Grid
+solution =
+    [[4,8,1,7,2,9,6,3,5],
+     [2,6,7,5,8,3,1,9,4],
+     [3,5,9,4,1,6,7,2,8],
+     [8,1,5,2,3,4,9,7,6],
+     [9,2,6,8,7,1,5,4,3],
+     [7,3,4,9,6,5,2,8,1],
+     [6,9,3,1,4,7,8,5,2],
+     [1,7,2,3,5,8,4,6,9],
+     [5,4,8,6,9,2,3,1,7]]
+
+partialSolutions :: [Puzzle]
+partialSolutions = let nextOne sq = withValueAt sq (valueAt sq solution)
+  in scanr nextOne killerPuzzle $ reverse $ emptySquares killerPuzzle
+    
